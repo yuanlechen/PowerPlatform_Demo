47a08a18bb13ba5f2c11bcfb2b0830b8d615dc72
yuanlechen <yuanlechen@live.cn>
2019-05-23 18:54:17 -0500

    Added vw_Position

---
+++ Database_Development/Database_Development/dbo/Views/vw_Position.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[vw_Position] AS
+
+SELECT
+	[or].[Email],
+	[po].[Title]
+FROM
+	[dbo].[DimEmployee] AS [or]
+	LEFT JOIN [dbo].[DimPosition] AS [po]
+		ON [or].[PositionKey] = [po].[PositionKey]
+		AND [po].[IsActive] = 1
