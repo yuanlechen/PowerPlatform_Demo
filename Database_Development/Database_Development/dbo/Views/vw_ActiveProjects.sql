bb80cd1336ab5d9afd83bd237983413a00dca31b
yuanlechen <yuanlechen@live.cn>
2019-05-24 16:07:26 -0500

    Added Active Project View

---
+++ Database_Development/Database_Development/dbo/Views/vw_ActiveProjects.sql
@@ -0,0 +1,27 @@
+﻿CREATE VIEW [dbo].[vw_ActiveProjects]
+	AS 
+	SELECT
+		[pj].[ProjectNumber]
+		,[pj].[ProjectName]
+		,[ph].[Phase]
+		,[dt].[FullDate]								AS [WorkStartDate]
+		,[pj].[CoordinatorKey]
+		,[cd].[FirstName] + ' ' +[cd].[LastName]		AS [Coordinator]
+		,[pj].[ProjectManagerKey]
+		,[pm].[FirstName] + ' ' +[pm].[LastName]		AS [ProjectManager]
+		,[pj].[TeamLeadKey]
+		,[tl].[FirstName] + ' ' +[tl].[LastName]		AS [TeamLead]
+	FROM 
+		[dbo].[FactProject] AS [pj]
+		LEFT JOIN [dbo].[DimPhase] AS [ph]
+			ON [pj].[PhaseKey] = [ph].[PhaseKey]
+		LEFT JOIN [dbo].[DimDate] AS [dt]
+			ON [pj].[WorkStartDateKey] = [dt].[DateKey]
+		LEFT JOIN [dbo].[DimEmployee] AS [cd]
+			ON [pj].[CoordinatorKey] = [cd].[EmployeeKey]
+		LEFT JOIN [dbo].[DimEmployee] AS [pm]
+			ON [pj].[ProjectManagerKey] = [pm].[EmployeeKey]
+		LEFT JOIN [dbo].[DimEmployee] AS [tl]
+			ON [pj].[TeamLeadKey] = [tl].[EmployeeKey]
+	WHERE
+		[IsComplete] = 0
